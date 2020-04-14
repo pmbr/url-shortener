6cc0a04f8d97ea52775033832d78a33c10949f7f
Paulo Rezende <paulo@pmbr.net>
2020-04-14 11:11:02 -0700

    class rename

--- src/main/java/org/project/URLShortenerApplication.java
+++ src/main/java/org/project/URLShortenerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlShortenerApplication {
+public class URLShortenerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UrlShortenerApplication.class, args);
+        SpringApplication.run(URLShortenerApplication.class, args);
     }
 
 }
