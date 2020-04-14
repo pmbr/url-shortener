9bd956deb63347601f886350e1c2267f7affcd44
Paulo Rezende <paulo@pmbr.net>
2020-04-14 11:42:42 -0700

    remove import

--- src/main/java/org/project/controller/IndexController.java
+++ src/main/java/org/project/controller/IndexController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.MalformedURLException;
 
 @Controller
 public class IndexController {
