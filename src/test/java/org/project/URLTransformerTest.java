afab99c5ca15124855394ec721d1cb519ed1173d
Paulo Rezende <paulo@pmbr.net>
2020-04-14 14:27:15 -0700

    url transformer test with mockito

--- src/test/java/org/project/URLTransformerTest.java
+++ src/test/java/org/project/URLTransformerTest.java
@@ -2,20 +2,21 @@ package org.project;
 
 import org.junit.Before;
 import org.junit.Test;
-
-import javax.servlet.http.HttpServletRequest;
-
-import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class URLTransformerTest {
 
     private URLTransformer urlTransformer;
 
     @Before
     public void setUp() {
-        urlTransformer = new URLTransformer();
+        urlTransformer = spy(new URLTransformer());
     }
 
     @Test
@@ -30,6 +31,13 @@ public class URLTransformerTest {
         String redirectURL = "https://store.apple.com";
         String returnedRedirectURL = urlTransformer.addProtocolIfAbsent(redirectURL);
         assertEquals("https://store.apple.com", returnedRedirectURL);
+    }
+
+    @Test
+    public void addHostToShortenedURL() {
+        doReturn("http://localhost:8080/").when(urlTransformer).extractRequestURL(any());
+        String shortenedUrlWithHost = urlTransformer.addHost(null, "1");
+        assertEquals("http://localhost:8080/1", shortenedUrlWithHost);
     }
 
 }
